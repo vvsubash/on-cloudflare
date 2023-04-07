ec11d7065643fbd8d6587ae427b106fd02af69cd
vvsubash <venkata.subash@fuelbuddy.in>
2023-04-07 18:02:32 +0530

    hjg

--- functions/api/hello2.ts
+++ functions/api/hello2.ts
@@ -1,14 +1,12 @@
 import "@cloudflare/workers-types"
 import { KVNamespace, PagesFunction } from "@cloudflare/workers-types"
 
-interface Env {
-  KV: KVNamespace
-}
 
-export const onRequest:PagesFunction<Env> (async(context: { request: any }) => {
+
+export const onRequest = async(context: { request: any }) => {
 
   const {request} = context
 
-  return new Response(request.cf.colo)
+  return new Response("request is being serverd from " + request.cf.colo)
 
-})
+}
