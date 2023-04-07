81197214b625a7bd91b0d92fb56b600db678f2fd
vvsubash <venkata.subash@fuelbuddy.in>
2023-04-07 16:08:32 +0530

    sdf

--- functions/api/hello2.ts
+++
@@ -1,9 +0,0 @@
-
-  export const onRequest = async (context) => {
-
-    const {request} =context
-    
-    console.log("hit hello2")
-    return new Response(request?.cf?.colo ?? "dfds");
-  }
-  
